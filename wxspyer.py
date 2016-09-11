c9d2e7d70aca4d6bde19c28b8aa04c13b9d42c7b
wususu <kevensusu@gmail.com>
2016-09-11 22:16:41 +0800

    Update wxspyer.py

--- wxspyer.py
+++ wxspyer.py
@@ -6,10 +6,11 @@ from multiprocessing import Pool
 from datetime import datetime
 from tools import *
 from urls import WXUrl
-from log_app import log
+import os
+# from log_app import log
 
 
-def GetHtml(url):
+def get_html(url):
     """
     提取html
     :param url:
@@ -19,90 +20,93 @@ def GetHtml(url):
     h = {'User-Agent': GetHeaders()}
     i = 0
     html = None
-    while html == None and i<5:
+    while html == None and i<10:
         try:
             rep = requests.get(url, headers=h)
             if rep.status_code == 200:
                 html = rep.content
                 i = 0
                 return html
-        except:
-            log.info("html重新提取",i,"URL:",url)
-            i+=1
+        except Exception as e:
+            print(e, "html重新提取", i, "URL:",url)
+            # log.info("html重新提取",i,"URL:",url)
+            i += 1
             html = None
             continue
 
 
-def GetPageUrl(url):
+def get_page_url(url):
     """
     提取文章url title
     :param url:
     :return: list
     """
     datas = []
+    list = []
     html = None
-    while(datas == [] or html == None):
-    #循环,避免推文信息刷新不出的情况
-        html = GetHtml(url)
+    while(datas is [] or html is None):
+        """
+        循环,避免推文信息刷新不出的情况
+        """
+        html = get_html(url)
         try:
             html = BeautifulSoup(html, "html.parser")
         except TypeError:
-            if html == None:
+            if html is None:
                 continue
         try:
             datas = html.find_all("h4")
         except TypeError:
             continue
         datas = datas[1:6]
-        list = []
         for data in datas:
-            dict = {}
-            dict["url"] = data.find("a").attrs['href']
-            dict["title"] = data.get_text()[1:-1]
-            list.append(dict)
-            del dict
-        print(list)
+            mdict = {}
+            mdict["url"] = data.find("a").attrs['href']
+            mdict["title"] = data.get_text()[1:-1]
+            list.append(mdict)
+            print(mdict['title'])
     del datas
     return list
 
 
-def GetArticle(dict):
+def get_article(dicts):
     """
     提取文章页面元素
-    :param dict:
+    :param dicts:
     :return: list
     """
     try:
-        html = GetHtml(dict['url'])
+        html = get_html(dicts['url'])
         html = BeautifulSoup(html, "html.parser")
-        dict['date'] = html.find("em", id='post-date').get_text() + " 00:00:00"
-        dict['post_author'] = html.find('a', id='post-user').get_text()
+        dicts['date'] = html.find("em", id='post-date').get_text() + " 00:00:00"
+        dicts['post_author'] = html.find('a', id='post-user').get_text()
         msg = html.find("div", id="js_content")
         imgs = msg.find_all("img")
         # imgs = imgs[1:-1]
         ulist = []
-        if imgs != []:
+        if imgs is not []:
             for img in imgs:
                 try:
                     ulist.append(img.attrs['data-src'])
-                except:
-                    log.error('图片地址解析出错:',img)
+                except Exception as e:
+                    print("error:", e)
+                    # log.error('图片地址解析出错:',img)
                     pass
         else:
             print("没有图片")
-        dict['content'] = msg.get_text(strip=True)
+        dicts['content'] = msg.get_text(strip=True)
     except AttributeError:
-        log.error("文章页面错误,跳过:",dict['title'],dict['url'])
+        # log.error("文章页面错误,跳过:",dict['title'],dict['url'])
         return {}
-    dict['img_url'] = ulist
-    print(dict['date'], dict['post_author'],dict['img_url'])
-    return dict
+    dicts['img_url'] = ulist
+    print(dicts['title'],dicts['post_author'])
+    return dicts
 
 
-def IntoDB(Alist):
+def into_db(lists):
     """
 
-    :param Alist:
+    :param lists:
     :return:
     """
     con = pymysql.connect(host='127.0.0.1', user='root',
@@ -111,76 +115,80 @@ def IntoDB(Alist):
     cur = con.cursor()
     values = []
     a = str(datetime.now())[:-7]
-    for i in Alist:
+    for i in lists:
         try:
             values.append((i['title'], i['post_author'], i['content'], i['url'], i['date'], a))
         except KeyError as e:
+            print("error:", e)
             continue
-    print(values)
     try:
         cur.executemany("insert ignore into wechat(title, post_auth, content, page_url, post_time, into_db)\
                           VALUES (%s, %s, %s, %s, %s, %s)", values)
         con.commit()
     except Exception as e:
+        print("error:", e)
         con.rollback()
-        log.error('插入数据库错误',e)
+        # log.error('插入数据库错误',e)
     con.close()
 
 
+def save_img(dicts):
+    """
 
-
-def SaveImg(dict):
+    :param dicts:
+    :return:
+    """
+    path = os.path.dirname(os.path.abspath('__file__'))
     try:
-        ilist = dict['img_url']
+        ilist = dicts['img_url']
     except KeyError as e:
         ilist = []
-        print("错误:::::::::::::::",e)
-    if ilist != []:
+        print("错误:::::::::::::::", e)
+    if ilist is not []:
         i = 0
         for url in ilist:
             i += 1
-            name = ''.join(dict['title'].split("/"))+str(i)+".png"
+            name = ''.join(dicts['title'].split("/"))+str(i)+".png"
             try:
-                urllib.request.urlretrieve(url, "img/"+ name)
-                print('图片',name)
-            except:
+                urllib.request.urlretrieve(url, path + "/img/" + name)
+            except Exception as e:
+                print("error:", e)
                 continue
     else:
         print("无图:::::::::::::::::::::::::::::::::")
         pass
 
-def ioimage(lists):
+
+def io_image(lists):
     pool = Pool(processes=4)
     for dicts in lists:
-        pool.apply_async(SaveImg, args=(dicts,))
+        pool.apply_async(save_img, args=(dicts,))
     pool.close()
     pool.join()
 
 
 def run():
-    log.info("微信爬虫启动")
+    print("微信爬虫启动",datetime.now()[:-7])
     urls = WXUrl()
     lists = []
     url_list = []
     for url in urls:
-        print("主::::::",url)
-        url_list += GetPageUrl(url)
-        print("文章::::::::",url_list)
+        print("主::::::" + url)
+        url_list += get_page_url(url)
 
     pool = Pool(processes=4)
-    lists += pool.map(GetArticle, url_list)
+    lists += pool.map(get_article, url_list)
     pool.close()
     pool.join()
-    log.info("爬虫抓取完毕")
-    IntoDB(lists)
-    log.info("成功存入数据库")
-    ioimage(lists)
-    log.info("图片保存完毕")
-    log.info("爬虫关闭")
+    print("爬虫抓取完毕")
+    into_db(lists)
+    print("成功存入数据库")
+    io_image(lists)
+    print("图片保存完毕")
+    print("爬虫关闭")
 
 
 if __name__ == '__main__':
     run()
-
 
 
